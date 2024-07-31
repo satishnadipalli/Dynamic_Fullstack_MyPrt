9a37c36308c450219f0f94a37c4cc4ff3f28d44f
Satish Nadipalli <satishnadipalli1@gmail.com>
2024-07-31 11:51:37 +0530

    added the UI functionality

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import { Fragment, useEffect, useState } from "react";
 import Cursor from "../src/layout/Cursor";
 import PreLoader from "../src/layout/PreLoader";
 import Image from "next/image";
-
+import AOS from "aos";
+import "aos/dist/aos.css"
 
 const Intro = () => {
   const [width,setWindowWidth] = useState();
@@ -20,6 +21,9 @@ const Intro = () => {
     // };
   }, []);
 
+  useEffect(()=>{
+    AOS.init({duration:1000,delay:1000}) 
+  })
   return (
     <Fragment>
       <Head>
@@ -34,26 +38,26 @@ const Intro = () => {
             <span className="anim" />
           </div>
           <div className="short_info">
-            <h3 style={{color:"gray",marginTop:"-40px"}} className="">Hey there!! Select Your Prefered Theme</h3>
+            <h3 style={{marginTop:"-40px"}} className="">Hey there!! Select Your Prefered Theme</h3>
           </div>
           <span className="intro_line" />
           <span className="intro_line_2" />
           <span className="intro_line_3" />
           <div style={{width:`${width}`,overflow:"hidden"}} className="demos">
             <div className="left">
-              <div className="desc desc1">
+              <div   className="desc desc1">
               <h3 className="titlesss">Light Theme</h3>
                 <a  href="index-white" target="_blank" >
-                  <img className="img1" src="img/intro/light.png" alt="image" />
+                  <img data-aos="slide-left" className="img1" src="img/intro/light.png" alt="image" />
                 </a>
                 <h3 className="tt">Light Theme</h3>
               </div>
               
             </div>
             <div className="right">
-              <div className="desc">
+              <div  className="desc"> 
                 <a href="index-dark" target="_blank" >
-                  <img className="img2" src="img/intro/dark.png" alt="image" />
+                  <img  data-aos="slide-right" className="img2" src="img/intro/dark.png" alt="image" />
                 </a>
                 <h3 className="title">Dark Theme</h3>
               </div>
