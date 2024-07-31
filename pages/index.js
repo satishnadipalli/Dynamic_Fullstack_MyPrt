17304cfa7b0a30df4aed7813709bd0f846b8a623
Satish Nadipalli <satishnadipalli1@gmail.com>
2024-07-31 11:39:31 +0530

    added the UI functionality

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ const Intro = () => {
             <div className="left">
               <div className="desc desc1">
               <h3 className="titlesss">Light Theme</h3>
-                <a className="intro_link" href="index-white" target="_blank" >
+                <a  href="index-white" target="_blank" >
                   <img className="img1" src="img/intro/light.png" alt="image" />
                 </a>
                 <h3 className="tt">Light Theme</h3>
@@ -52,7 +52,7 @@ const Intro = () => {
             </div>
             <div className="right">
               <div className="desc">
-                <a a className="intro_link" href="index-dark" target="_blank" >
+                <a href="index-dark" target="_blank" >
                   <img className="img2" src="img/intro/dark.png" alt="image" />
                 </a>
                 <h3 className="title">Dark Theme</h3>
